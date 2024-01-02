cd1e8b6e9870c0cfc0833860fd0f68fa402db999
Arif uz zaman <arifuzzaman959@gmail.com>
2024-01-02 12:53:41 +0600

    React context API initiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
 import './App.css'
 import Parent from './components/Parent'
+import CounterContext from './context/CounterContext'
 
 function App() {
  
 
   return (
-    <>
+    <CounterContext>
       <Parent/>
-    </>
+    </CounterContext>
   )
 }
 
--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { COUNTER_CONTEXT } from '../context/CounterContext';
 
 const Action = () => {
-    const [count, setCount] = useState(0)
+    const {count, setCount} = useContext(COUNTER_CONTEXT)
     return (
         <div className='mt-8 text-center' >
                 <button onClick={() => setCount(count + 1)} className='bg-green-800 text-white px-2 py-1 rounded-md mr-3'>Increment</button>
                 <span>{count}</span>
                 <button onClick={()=>setCount(count - 1)} className='bg-red-800 text-white px-2 py-1 rounded-md ml-3'>Decrement</button>
-            </div>
+        </div>
     );
 };
 
